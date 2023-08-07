f80eb282f4a1cbd44675d5f6505b6fbf27dec739
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-08-07 21:19:03 +0300

    TDD for .net core api

---
+++ eshop/eshop.API/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eshop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public IActionResult Search(string name)
+        {
+            return Ok(new { message = "This is for success...." });
+        }
+    }
+}
---
+++ eshop/eshop.Tests/ProductsControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using eshop.API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eshop.Tests
+{
+    public class ProductsControllerTests
+    {
+        /*
+         * Bir müşteri olarak
+         * Uygun ürünleri aramak istiyorum 
+         * Böylece ürünleri görebilir ve sepete ekleyebilirim.
+         * 
+         * Given -> Bir ürün adı girdiğimde
+         * When -> Ürün arama işlemini başlattığımda
+         * Then -> Ürün adında girdiğim isimdeki ürünler listelenecek.
+         * 
+         *
+         */
+        ProductsController productsController;
+        public ProductsControllerTests()
+        {
+            productsController = new ProductsController();
+        }
+
+        //[Fact]
+        //public void It_Exists()
+        //{
+        //    var controller = new eshop.API.Controllers.ProductsController();
+        //    var response = controller.Search("word");
+        //}
+
+        [Fact]
+        public void It_Return_Object_Result()
+        {
+            var response = productsController.Search("name");
+            Assert.NotNull(response);
+            Assert.IsType<OkObjectResult>(response);
+        }
+
+
+    }
+}
